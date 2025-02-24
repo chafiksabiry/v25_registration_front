6c854f345b627372d6b8f63aacc3466cb6be2ff2
karouma-oum <omaimaayoube149@gmail.com>
2025-02-24 12:01:54 +0100

    callbacks modifications

--- src/components/LinkedInCallback.tsx
+++ src/components/LinkedInCallback.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { useEffect } from 'react';
 import { useSearchParams, useNavigate } from 'react-router-dom';
 import { handleLinkedInCallback } from '../utils/Linkedin';
